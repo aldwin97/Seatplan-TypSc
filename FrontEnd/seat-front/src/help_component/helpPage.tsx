eb724d399662aa178524b07cfc2e1b42c162a2da
Nymwhel Bernardo <113109523+nbernardo1411@users.noreply.github.com>
2023-07-04 07:02:35 +0800

    Update - Help Center

--- FrontEnd/seat-front/src/help_component/helpPage.tsx
+++ FrontEnd/seat-front/src/help_component/helpPage.tsx
@@ -14,10 +14,27 @@ const HelpPage = () => {
       <div className={styles.container2}>
         <form className={styles.form2}>
           {/* Form content */}
-          <div className={styles.img}>Our Support Center</div>
+          <div className={styles.img}>OUR SUPPORT CENTER<div className={styles.searchBar}>
+          <input type="text" placeholder="Have a question? Search here!" className={styles.searchInput} />
+          <button className={styles.searchButton} type="submit">
+            <FaSearch />
+          </button>
+        </div></div>
+          
           <button className={styles.sub} type="submit" onClick={helpPageHandleClick}>
             SIGN IN
-          </button>
+          </button> 
+          <div className={styles.scrollable}>
+          <div className={styles.text1}>FREQUENTLY ASKED QUESTIONS</div>
+          <div className={styles.text2}>General Questions</div>
+          <div className={styles.text3}>•How do I delete My Account?</div>
+          <form className={styles.form1}></form>
+          <div className={styles.text3}>•How can I update my seat?</div>
+          <form className={styles.form1}></form>
+          <div className={styles.text3}>•How can I recover my Account?</div>
+          <form className={styles.form1}></form>
+          </div>
+
         </form>
       </div>
     </>

75a28c85bd634961883af6a06879f638c858cdc4
Nymwhel Bernardo <113109523+nbernardo1411@users.noreply.github.com>
2023-07-07 15:31:02 +0800

    up

--- FrontEnd/seat-front/src/admin_component/adminMembersPage.tsx
+++ FrontEnd/seat-front/src/admin_component/adminMembersPage.tsx
@@ -27,7 +27,6 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import { useNavigate } from 'react-router-dom';
 import './adminMembersPage.css';
 
-
 const useStyles = makeStyles({
   container: {
     width: '200px',
@@ -39,6 +38,7 @@ const useStyles = makeStyles({
     marginRight: '10px',
   },
 });
+
 interface User {
   id: number;
   firstName: string;
@@ -57,10 +57,9 @@ const AdminMembersPage: React.FC = () => {
   const [selectedUsers, setSelectedUsers] = useState<number[]>([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [addUserDialogOpen, setAddUserDialogOpen] = useState(false);
-  const [userInfoDialogOpen, setUserInfoDialogOpen] = useState(false);  
+  const [userInfoDialogOpen, setUserInfoDialogOpen] = useState(false);
   const [selectedUser, setSelectedUser] = useState<User | null>(null);
-  const [users, setUsers] = useState<User[]>([
-  ]);
+  const [users, setUsers] = useState<User[]>([]);
   const [newUser, setNewUser] = useState<User>({
     id: 0,
     firstName: '',
@@ -75,10 +74,7 @@ const AdminMembersPage: React.FC = () => {
     created_by: '',
   });
   const [perPage, setPerPage] = useState(8);
-const navigate = useNavigate();
-  const handleUserClick = () => {
-    navigate('/viewInformationPage');
-  };
+  const navigate = useNavigate();
 
   const handleCloseDialog = () => {
     setUserInfoDialogOpen(false);
@@ -97,26 +93,39 @@ const navigate = useNavigate();
     });
   };
 
+  const handleViewUserInfo = (userId: number) => {
+    const user = users.find((user) => user.id === userId);
+    if (user) {
+      setSelectedUser(user);
+      setUserInfoDialogOpen(true);
+    }
+  };
+
+  const handleUserClick = (userId: number) => {
+    handleViewUserInfo(userId);
+  };
+
   const handleDeleteSelected = () => {
     const remainingUsers = users.filter((user) => !selectedUsers.includes(user.id));
     setSelectedUsers([]);
     setCurrentPage(1);
     setUsers(remainingUsers);
   };
+
   const handleAddUser = () => {
     setAddUserDialogOpen(true); // Set the flag to open the dialog
   };
-  
+
   const handleAddUsers = () => {
     const currentTime = new Date().toISOString().slice(0, 19).replace('T', ' ');
-  
+
     const updatedUser: User = {
       ...newUser,
       id: users.length + 1,
       created_time: currentTime,
       created_by: 'Admin',
     };
-  
+
     setUsers([...users, updatedUser]);
     setNewUser({
       id: 0,
@@ -131,23 +140,24 @@ const navigate = useNavigate();
       created_time: '',
       created_by: '',
     });
-  
-    setAddUserDialogOpen(false); // Set the flag to open the dialog
+
+    setAddUserDialogOpen(false); // Set the flag to close the dialog
   };
-  
+
   const handlePageChange = (_: React.ChangeEvent<unknown>, value: number) => {
     setCurrentPage(value);
   };
 
   const perPageOptions = [2, 5, 10];
 
-  // Define handlePerPageChange function
   const handlePerPageChange = (event: SelectChangeEvent<number>) => {
     const value = event.target.value as number;
     setPerPage(value);
     setCurrentPage(1);
   };
+
   const [searchText, setSearchText] = useState('');
+
   const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchText(event.target.value);
   };
@@ -159,49 +169,67 @@ const navigate = useNavigate();
       user.username.toLowerCase().includes(searchText.toLowerCase())
   );
 
-  // Calculate the index of the first and last user on the current page
   const indexOfLastUser = currentPage * perPage;
   const indexOfFirstUser = indexOfLastUser - perPage;
   const currentUsers = filteredUsers.slice(indexOfFirstUser, indexOfLastUser);
 
-  
+  const [editMode, setEditMode] = useState(false);
+  const [editedUser, setEditedUser] = useState<User | null>(null);
 
+  const handleEditUser = () => {
+    setEditMode(true);
+    setEditedUser(selectedUser);
+  };
 
+  const handleSaveUser = () => {
+    if (editedUser) {
+      const updatedUsers = users.map((user) => {
+        if (user.id === editedUser.id) {
+          return editedUser;
+        }
+        return user;
+      });
+
+      setUsers(updatedUsers);
+      setEditMode(false);
+      setEditedUser(null);
+    }
+  };
 
   return (
-    <div className='container'>
-  <Box className='action-buttons' display="flex" justifyContent="flex-end">
-  <IconButton className={`delete-button ${selectedUsers.length > 0 ? 'active' : ''}`} color="primary" onClick={handleDeleteSelected} disabled={selectedUsers.length === 0}>
-  <DeleteIcon />
-</IconButton>
-    <Button className='add-user-button' color="primary" variant="contained" onClick={handleAddUser}>
-      Add User
-    </Button>
-    
-  </Box>
-  <div className='title'><Typography className='title-main' variant="h5" gutterBottom>
-    USER INFORMATION
-  </Typography><div className="menu-label">Items per Page:</div>
-  <Select className="select-container" value={perPage} onChange={handlePerPageChange}>
-    {perPageOptions.map((option) => (
-      <MenuItem className="menu-container" key={option} value={option}>
-        {option}
-      </MenuItem>
-  ))}
-</Select>
-  <div className='search-bar'>
-    <TextField
-        className='search-bar'
-        label="Search"
-        variant="outlined"
-        value={searchText}
-        onChange={handleSearch}
-      /></div>
-      
+    <div className="container">
+      <Box className="action-buttons" display="flex" justifyContent="flex-end">
+        <IconButton
+          className={`delete-button ${selectedUsers.length > 0 ? 'active' : ''}`}
+          color="primary"
+          onClick={handleDeleteSelected}
+          disabled={selectedUsers.length === 0}
+        >
+          <DeleteIcon />
+        </IconButton>
+        <Button className="add-user-button" color="primary" variant="contained" onClick={handleAddUser}>
+          Add User
+        </Button>
+      </Box>
+      <div className="title">
+        <Typography className="title-main" variant="h5" gutterBottom>
+          USER INFORMATION
+        </Typography>
+        <div className="menu-label">Items per Page:</div>
+        <Select className="select-container" value={perPage} onChange={handlePerPageChange}>
+          {perPageOptions.map((option) => (
+            <MenuItem className="menu-container" key={option} value={option}>
+              {option}
+            </MenuItem>
+          ))}
+        </Select>
+        <div className="search-bar">
+          <TextField className="search-bar" label="Search" variant="outlined" value={searchText} onChange={handleSearch} />
+        </div>
       </div>
-  <TableContainer className='table-container' component={Paper}>
+      <TableContainer className="table-container" component={Paper}>
     <Table>
-      <TableHead className='table-header'>
+      <TableHead className="table-header">
         <TableRow>
           <TableCell></TableCell>
           <TableCell>Name</TableCell>
@@ -219,7 +247,7 @@ const navigate = useNavigate();
               />
             </TableCell>
             <TableCell>
-              <a className='user-link' href="#" onClick={handleUserClick}>
+              <a className="user-link" href="#" onClick={() => handleUserClick(user.id)}>
                 {`${user.firstName} ${user.lastName}`}
               </a>
             </TableCell>
@@ -230,15 +258,102 @@ const navigate = useNavigate();
       </TableBody>
     </Table>
   </TableContainer>
-  <Box className='pagination-container' display="flex" justifyContent="center" marginTop={2}>
-  <Pagination
-          count={Math.ceil(users.length / perPage)}
-          page={currentPage}
-          onChange={handlePageChange}
-          color="primary"
-        />
-        
-      </Box>
+  <Box className="pagination-container" display="flex" justifyContent="center" marginTop={2}>
+    <Pagination count={Math.ceil(filteredUsers.length / perPage)} page={currentPage} onChange={handlePageChange} color="primary" />
+  </Box>
+  <Dialog open={userInfoDialogOpen} onClose={handleCloseDialog} className="user-info-dialog">
+    <DialogTitle className="user-info-dialog-title">User Information</DialogTitle>
+    <DialogContent className="user-info-content">
+      {selectedUser && (
+        <DialogContentText className="user-info-dialog-content-text">
+        <strong className="user-info-label">Name:</strong>
+        {editMode ? (
+          <TextField
+            className="user-info-value"
+            value={editedUser?.firstName}
+            onChange={(e) =>
+              setEditedUser((prevEditedUser: User | null) => ({
+                ...prevEditedUser!,
+                username: e.target.value
+              }))
+            }
+            
+          />
+        ) : (
+          <span className="user-info-value">{`${selectedUser.firstName} ${selectedUser.lastName}`}</span>
+        )}
+        <br />
+        <strong className="user-info-label">Username:</strong>
+        {editMode ? (
+          <TextField
+            className="user-info-value"
+            value={editedUser?.username}
+            onChange={(e) =>
+              setEditedUser((prevEditedUser: User | null) => ({
+                ...prevEditedUser!,
+                username: e.target.value
+              }))
+            }
+            
+          />
+        ) : (
+          <span className="user-info-value">{selectedUser.username}</span>
+        )}
+        <br />
+        <strong className="user-info-label">Email:</strong>
+        {editMode ? (
+          <TextField
+            className="user-info-value"
+            value={editedUser?.email}
+            onChange={(e) =>
+              setEditedUser((prevEditedUser: User | null) => ({
+                ...prevEditedUser!,
+                email: e.target.value
+              }))
+            }
+            
+          />
+        ) : (
+          <span className="user-info-value">{selectedUser.email}</span>
+        )}
+        <br />
+        <strong className="user-info-label">Contact:</strong>{" "}
+        <span className="user-info-value">{selectedUser.contact}</span> <br />
+        <strong className="user-info-label">Password:</strong>{" "}
+        <span className="user-info-value">
+          {selectedUser.password ? "********" : ""}
+        </span>{" "}
+        <br />
+        <strong className="user-info-label">UserType:</strong>{" "}
+        <span className="user-info-value">{selectedUser.usertype}</span> <br />
+        <strong className="user-info-label">Position:</strong>{" "}
+        <span className="user-info-value">{selectedUser.position}</span> <br />
+        <strong className="user-info-label">Created At:</strong>{" "}
+        <span className="user-info-value">{selectedUser.created_time}</span> <br />
+        <strong className="user-info-label">Created By:</strong>{" "}
+        <span className="user-info-value">{selectedUser.created_by}</span>
+      </DialogContentText>
+      
+    )}
+  </DialogContent>
+  <DialogActions className="user-info-actions">
+  {editMode ? (
+    <Button onClick={handleSaveUser} color="primary" className="user-info-dialog-save-button">
+      Save
+    </Button>
+  ) : (
+    <Button onClick={handleEditUser} color="primary" className="user-info-dialog-edit-button">
+      Edit
+    </Button>
+  )}
+  <Button onClick={handleCloseDialog} color="primary" className="user-info-dialog-close-button">
+    Close
+  </Button>
+</DialogActions>
+
+</Dialog>
+
+
   <Dialog open={addUserDialogOpen || newUser.id !== 0} onClose={() => setNewUser({ ...newUser, id: 0 })} className='add-user-dialog'>
     <DialogTitle>Add User</DialogTitle>
     <DialogContent className='add-user-dialog-content'>

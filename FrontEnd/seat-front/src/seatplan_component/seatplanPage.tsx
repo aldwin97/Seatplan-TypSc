bfd0efae454c9ae30fb3f3b5095868e568673e62
Nymwhel Bernardo <113109523+nbernardo1411@users.noreply.github.com>
2023-07-10 10:27:40 +0800

    updates

--- FrontEnd/seat-front/src/seatplan_component/seatplanPage.tsx
+++ FrontEnd/seat-front/src/seatplan_component/seatplanPage.tsx
@@ -1,84 +1,62 @@
-import { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser, faBell, faChartBar, faUsers, faProjectDiagram, faPowerOff, faFaceSmile } from '@fortawesome/free-solid-svg-icons';
-import styles from './seatplanPage.module.css';
-import { makeStyles } from '@material-ui/core/styles';
 import { Grid, Paper } from '@material-ui/core';
-import { DragDropContext, Droppable, Draggable, DropResult } from 'react-beautiful-dnd';
+import { DragDropContext, Droppable, Draggable, DropResult, DraggableProvided, DraggableStateSnapshot, DroppableProvided } from 'react-beautiful-dnd';
+import styles from './seatplanPage.module.css';
+
+interface Seat {
+  id: string;
+  label: string;
+}
+
+const initialSeats: Seat[] = [
+  { id: 'seat1', label: 'Seat 1' },
+  { id: 'seat2', label: 'Seat 2' },
+  { id: 'seat3', label: 'Seat 3' },
+  { id: 'seat4', label: 'Seat 4' },
+  { id: 'seat5', label: 'Seat 5' },
+];
 
 function SeatplanPage() {
+  const navigate = useNavigate();
 
+  const dashboardPageHandleClick = () => {
+    navigate('/DashboardPage');
+  };
 
-    const navigate = useNavigate();
+  const adminPageHandleClick = () => {
+    navigate('/AdminPage');
+  };
 
-    const dashboardPageHandleClick = () => {
-      navigate('/DashboardPage');
-    };
-    const adminPageHandleClick = () => {
-      navigate('/AdminPage');
-    };
-  
-    const logInPageHandleClick = () => {
-      navigate('/');
-    };
-  
-    const [isDropdownOpen, setDropdownOpen] = useState(false);
-    const [isProfileDropdownOpen, setProfileDropdownOpen] = useState(false);
-    const [currDate, setCurrDate] = useState('');
-    const [currTime, setCurrTime] = useState('');
-  
-    const toggleDropdown = () => {
-      setDropdownOpen(!isDropdownOpen);
-    };
-  
-    const toggleProfileDropdown = () => {
-      setProfileDropdownOpen(!isProfileDropdownOpen);
-    };
-  
-    const useStyles = makeStyles((theme) => ({
-        root: {
-          flexGrow: 1,
-          padding: theme.spacing(2),
-        },
-        paper: {
-          padding: theme.spacing(2),
-          textAlign: 'center',
-          color: theme.palette.text.secondary,
-          minHeight: '100px',
-        },
-      }));
-      
-      interface Seat {
-        id: string;
-        label: string;
-      }
-      
-      const initialSeats: Seat[] = [
-        { id: 'seat1', label: 'Seat 1' },
-        { id: 'seat2', label: 'Seat 2' },
-        { id: 'seat3', label: 'Seat 3' },
-        { id: 'seat4', label: 'Seat 4' },
-        { id: 'seat5', label: 'Seat 5' },
-      ];
-      
-        const classes = useStyles();
-        const [seats, setSeats] = useState<Seat[]>(initialSeats);
-      
-        const handleDragEnd = (result: DropResult) => {
-          if (!result.destination) {
-            return;
-          }
-      
-          const updatedSeats = [...seats];
-          const [draggedSeat] = updatedSeats.splice(result.source.index, 1);
-          updatedSeats.splice(result.destination.index, 0, draggedSeat);
-          setSeats(updatedSeats);
-        };
-      
+  const logInPageHandleClick = () => {
+    navigate('/');
+  };
 
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
+  const [isProfileDropdownOpen, setProfileDropdownOpen] = useState(false);
 
+  const toggleDropdown = () => {
+    setDropdownOpen(!isDropdownOpen);
+  };
 
+  const toggleProfileDropdown = () => {
+    setProfileDropdownOpen(!isProfileDropdownOpen);
+  };
+
+  const [seats, setSeats] = useState<Seat[]>(initialSeats);
+
+  const handleDragEnd = (result: DropResult) => {
+    if (!result.destination) {
+      return;
+    }
+
+    const updatedSeats = [...seats];
+    const [draggedSeat] = updatedSeats.splice(result.source.index, 1);
+    updatedSeats.splice(result.destination.index, 0, draggedSeat);
+    setSeats(updatedSeats);
+  };
 
   return (
     <div className={styles.container}>
@@ -119,44 +97,40 @@ function SeatplanPage() {
             <FontAwesomeIcon icon={faPowerOff} className={styles.icon} />
             Logout
           </button>
-          
         </div>
       )}
 
-      <button className={styles.notif}>
-        <FontAwesomeIcon icon={faBell} />
-      </button>
-      <div className={classes.root}>
-      <DragDropContext onDragEnd={handleDragEnd}>
-        <Droppable droppableId="seatPlan">
-          {(provided) => (
-            <div {...provided.droppableProps} ref={provided.innerRef}>
-              <Grid container spacing={2}>
-                {seats.map((seat, index) => (
-                  <Draggable key={seat.id} draggableId={seat.id} index={index}>
-                    {(provided) => (
-                      <Grid item xs={6} sm={4} md={3}>
-                        <Paper
-                          className={classes.paper}
-                          ref={provided.innerRef}
-                          {...provided.draggableProps}
-                          {...provided.dragHandleProps}
-                        >
-                          {seat.label}
-                        </Paper>
-                      </Grid>
-                    )}
-                  </Draggable>
-                ))}
-              </Grid>
-              {provided.placeholder}
-            </div>
-          )}
-        </Droppable>
-      </DragDropContext>
+      <div className={styles.root}>
+        <DragDropContext onDragEnd={handleDragEnd}>
+          <Droppable droppableId="seatPlan">
+            {(provided: DroppableProvided) => (
+              <div {...provided.droppableProps} ref={provided.innerRef}>
+                <Grid container spacing={2}>
+                  {seats.map((seat, index) => (
+                    <Draggable key={seat.id} draggableId={seat.id} index={index}>
+                      {(provided: DraggableProvided, snapshot: DraggableStateSnapshot) => (
+                        <Grid item xs={6} sm={4} md={3}>
+                          <Paper
+                            className={styles.paper}
+                            ref={provided.innerRef}
+                            {...provided.draggableProps}
+                            {...provided.dragHandleProps}
+                          >
+                            {seat.label}
+                          </Paper>
+                        </Grid>
+                      )}
+                    </Draggable>
+                  ))}
+                </Grid>
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
+      </div>
     </div>
-</div>
-  )
+  );
 }
 
-export default SeatplanPage
+export default SeatplanPage;

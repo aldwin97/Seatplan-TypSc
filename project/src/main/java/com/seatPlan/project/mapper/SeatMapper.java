8a08aea8b53d4eb9ce1449b2fe59599fdf08fca0
Kenneth Christian Gutierrez <kennethchristianb.gutierrez@gmail.com>
2023-07-11 09:31:18 +0800

    API

--- project/src/main/java/com/seatPlan/project/mapper/SeatMapper.java
+++ project/src/main/java/com/seatPlan/project/mapper/SeatMapper.java
@@ -1,5 +1,15 @@
 package com.seatPlan.project.mapper;
 
-public class SeatMapper {
-    
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import com.seatPlan.project.model.SeatModel;
+
+@Mapper
+public interface SeatMapper {
+
+    @Select("SELECT * FROM table_seat")
+    List<SeatModel> getAllSeats();
 }
--- project/src/main/java/com/seatPlan/project/service/SeatService.java
+++ project/src/main/java/com/seatPlan/project/service/SeatService.java
@@ -1,8 +1,23 @@
 package com.seatPlan.project.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.seatPlan.project.mapper.SeatMapper;
+import com.seatPlan.project.model.SeatModel;
 
 @Service
 public class SeatService {
-    
+    private final SeatMapper seatMapper;
+
+   
+    public SeatService(@Autowired SeatMapper seatMapper) {
+        this.seatMapper = seatMapper;
+    }
+
+    public List<SeatModel> getAllSeats() {
+        return seatMapper.getAllSeats();
+    }
 }

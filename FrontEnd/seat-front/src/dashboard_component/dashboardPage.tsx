23b1d9fde34ff00615da2e4e03b564f4d7409215
Abigail Anne Calaminos <57781516+Abby367@users.noreply.github.com>
2023-07-17 13:48:59 +0800

    Front-end(Dashboard)

--- FrontEnd/seat-front/src/dashboard_component/dashboardPage.tsx
+++ FrontEnd/seat-front/src/dashboard_component/dashboardPage.tsx
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
 import SwipeableDrawer from '@mui/material/SwipeableDrawer';
-import { Button } from '@mui/material';
+import { Button, Card, CardActions, CardContent, Typography } from '@mui/material';
 import styles from './dashboardPage.module.css';
 import { Dashboard, Work, Menu } from '@mui/icons-material';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser, faPowerOff, faSmile } from '@fortawesome/free-solid-svg-icons';
+import { faUser, faBell, faChartBar, faUsers, faProjectDiagram, faPowerOff, faFaceSmile } from '@fortawesome/free-solid-svg-icons';
 import { useNavigate } from 'react-router-dom';
+
+
+
 
 const DashboardPage: React.FC = () => {
   const navigate = useNavigate();
@@ -48,10 +51,8 @@ const DashboardPage: React.FC = () => {
   return (
     <>
       <body>
-        <div
-          className={`${styles['connect-container']} ${styles['align-content-stretch']} ${styles['d-flex']} ${styles['flex-wrap']}`}
-        >
-          <div className={styles['page-header']}>
+        
+          <div >
             {(['left'] as const).map((anchor) => (
               <React.Fragment key={anchor}>
                 <Button onClick={toggleDrawer(anchor, true)}>
@@ -105,30 +106,100 @@ const DashboardPage: React.FC = () => {
                 </SwipeableDrawer>
               </React.Fragment>
             ))}
-            <div className={styles['page-header']}>
-              <button
-                className={`${styles.dropdown} ${isProfileDropdownOpen ? styles.open2 : ''}`}
-                onClick={toggleProfileDropdown}
-              >
-                <FontAwesomeIcon icon={faUser} />
-              </button>
+            
+            <button className={`${styles.profile} ${isProfileDropdownOpen ? styles.open2 : ''}`} onClick={toggleProfileDropdown}>
+        <FontAwesomeIcon icon={faUser} />
+      </button>
 
-              {isProfileDropdownOpen && (
-                <div className={styles.dropdownMenu}>
-                  <button className={styles.dropdownItem}>
-                    <FontAwesomeIcon icon={faSmile} className={styles.icon} />
-                    Profile
-                  </button>
-
-                  <button onClick={logInPageHandleClick} className={styles.dropdownItem}>
-                    <FontAwesomeIcon icon={faPowerOff} className={styles.icon} />
-                    Logout
-                  </button>
-                </div>
-              )}
-            </div>
-          </div>
+      {isProfileDropdownOpen && (
+        <div className={styles.dropdownMenu2}>
+          <button className={styles.sub}>
+            <FontAwesomeIcon icon={faFaceSmile} className={styles.icon} />
+            Profile
+          </button>
+          <button onClick={logInPageHandleClick} className={styles.sub}>
+            <FontAwesomeIcon icon={faPowerOff} className={styles.icon} />
+            Logout
+          </button>
+          
         </div>
+      )}
+
+      <button className={styles.notif}>
+        <FontAwesomeIcon icon={faBell} />
+      </button>
+
+
+      <div className={styles.container2}>
+        <form className={styles.main2}>
+
+          <form className={styles.form3}>
+
+
+         
+        <form className={styles.form2}>
+        <form className={styles.form}>
+
+<div className={styles.card}>
+  <svg className={styles.cardimg}></svg>
+  <div className={styles.cardtitle}>HELLO (USERNAME)</div>
+  <div className={styles.cardtext}>Welcome Back! Always check you Notifications!</div>
+  <div className={styles.date}>Today is</div>
+  <div className={styles.time}></div>
+</div>
+</form>
+          <div className={styles.card2}>
+            <svg className={styles.cardimg2}></svg>
+            <div className={styles.cardtitle2}>TOTAL SEAT</div>
+          </div>
+
+          <div className={styles.card3}>
+            <svg className={styles.cardimg3}></svg>
+            <div className={styles.cardtitle3}>OCCUPIED SEAT</div>
+          </div>
+          
+          <div className={styles.card4}>
+            <svg className={styles.cardimg4}></svg>
+            <div className={styles.cardtitle4}>AVAILABLE SEAT</div>
+          </div>
+          <div>
+          <div className={styles.card5}>
+    <svg className={styles.cardimg5}></svg>
+    <div className={styles.cardtitle5}>SEAT CONDITIONS</div>
+  </div>
+          </div>
+          <div className={styles.card6}>
+            <svg className={styles.cardimg6}></svg>
+            <div className={styles.cardtitle6}>TOTAL EMPLOYEE</div>
+          </div>
+
+          <div className={styles.card7}>
+            <svg className={styles.cardimg7}></svg>
+            <div className={styles.cardtitle7}>TRAINEE</div>
+          </div>
+
+          <div className={styles.card8}>
+            <svg className={styles.cardimg8}></svg>
+            <div className={styles.cardtitle8}>REGULAR EMPLOYEE</div>
+          </div>
+          
+          </form>
+
+          <form className={styles.form4}>
+         
+          <div className={styles.scrollable}>
+          <div className={styles.card9}>
+            <svg className={styles.cardimg9}></svg>
+            <div className={styles.cardtitle9}>RECENT COMMENTS</div>
+          </div></div>
+    </form>
+          
+        </form>
+        </form>
+ 
+        </div>
+        </div>
+       
       </body>
     </>
   );

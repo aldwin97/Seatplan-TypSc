8691dffcf5620cb32cbe225aad23a3adaeb804b1
aldwin97 <rivera.aldwin.f@gmail.com>
2023-08-08 09:39:27 +0800

    Migration to java10

--- project/src/main/java/com/seatPlan/project/security/TokenUtils.java
+++ project/src/main/java/com/seatPlan/project/security/TokenUtils.java
@@ -1,53 +1,53 @@
-package com.seatPlan.project.security;
+// package com.seatPlan.project.security;
 
-import java.security.Key;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Collections;
+// import java.security.Key;
+// import java.util.Date;
+// import java.util.HashMap;
+// import java.util.Map;
+// import java.util.Collections;
 
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+// import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.JwtException;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.security.Keys;
+// import io.jsonwebtoken.Claims;
+// import io.jsonwebtoken.JwtException;
+// import io.jsonwebtoken.Jwts;
+// import io.jsonwebtoken.SignatureAlgorithm;
+// import io.jsonwebtoken.security.Keys;
 
 
-public class TokenUtils {
-    private final static Key ACCESS_TOKEN_SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
-    private final static Long ACCESS_TOKEN_VALIDITY_SECONDS = 2_592_000L;
+// public class TokenUtils {
+//     private final static Key ACCESS_TOKEN_SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+//     private final static Long ACCESS_TOKEN_VALIDITY_SECONDS = 2_592_000L;
 
-    public static String createToken( String username){
-        long expirationTime = ACCESS_TOKEN_VALIDITY_SECONDS * 1_000;
-        Date expirationDate = new Date(System.currentTimeMillis() + expirationTime);
+//     public static String createToken( String username){
+//         long expirationTime = ACCESS_TOKEN_VALIDITY_SECONDS * 1_000;
+//         Date expirationDate = new Date(System.currentTimeMillis() + expirationTime);
 
-        Map<String, Object> extra = new HashMap<>();
-        extra.put("username", username); 
+//         Map<String, Object> extra = new HashMap<>();
+//         extra.put("username", username); 
 
-        return Jwts.builder()
-                .setSubject(username)
-                .setExpiration(expirationDate)
-                .addClaims(extra)
-                .signWith(ACCESS_TOKEN_SECRET_KEY)
-                .compact();
+//         return Jwts.builder()
+//                 .setSubject(username)
+//                 .setExpiration(expirationDate)
+//                 .addClaims(extra)
+//                 .signWith(ACCESS_TOKEN_SECRET_KEY)
+//                 .compact();
 
-}
+// }
 
-public static UsernamePasswordAuthenticationToken getAuthentication(String token) {
-     try{
-         Claims claims = Jwts.parserBuilder()
-                 .setSigningKey(ACCESS_TOKEN_SECRET_KEY)
-                 .build()
-                 .parseClaimsJws(token)
-                 .getBody();
-                 String username = claims.getSubject();
-                 return new UsernamePasswordAuthenticationToken(username,null, Collections.emptyList());
-}catch(JwtException e){
-    return null;
-}
+// public static UsernamePasswordAuthenticationToken getAuthentication(String token) {
+//      try{
+//          Claims claims = Jwts.parserBuilder()
+//                  .setSigningKey(ACCESS_TOKEN_SECRET_KEY)
+//                  .build()
+//                  .parseClaimsJws(token)
+//                  .getBody();
+//                  String username = claims.getSubject();
+//                  return new UsernamePasswordAuthenticationToken(username,null, Collections.emptyList());
+// }catch(JwtException e){
+//     return null;
+// }
 
-}
-}
+// }
+// }
 

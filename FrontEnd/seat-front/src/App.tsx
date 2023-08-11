05606e7ab603bdbf024bf678f5da0b17faa15332
Nymwhel Bernardo <113109523+nbernardo1411@users.noreply.github.com>
2023-08-11 14:33:46 +0800

    up

--- FrontEnd/seat-front/src/App.tsx
+++ FrontEnd/seat-front/src/App.tsx
@@ -9,58 +9,23 @@ import SeatplanPage from './seatplan_component/seatplanPage';
 import ProfilePage from './profile_component/profilePage';
 import ProjectPage from './project_component/projectPage';
 import MachinePage from './admin_component/machinetablePage';
-import ProtectedRoute from './ProtectedRoute'; // Make sure the path is correct
+
 import './App.css';
-  
+
 function App(): JSX.Element {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          {/* Public route for login */}
           <Route path="/" element={<LoginPage />} />
-
-          {/* Protected routes */}
-          <ProtectedRoute
-            path="/viewSeatPage"
-            allowedUserTypes={[1, 2]}
-            element={<ViewSeatPage />}
-          />
-          <ProtectedRoute
-            path="/dashboardPage"
-            allowedUserTypes={[1, 2]}
-            element={<DashboardPage />}
-          />
-          <ProtectedRoute
-            path="/helpPage"
-            allowedUserTypes={[1, 2]}
-            element={<HelpPage />}
-          />
-          <ProtectedRoute
-            path="/projectPage"
-            allowedUserTypes={[1, 2]}
-            element={<ProjectPage />}
-          />
-          <ProtectedRoute
-            path="/seatplanPage"
-            allowedUserTypes={[1, 2]}
-            element={<SeatplanPage />}
-          />
-          <ProtectedRoute
-            path="/adminPage"
-            allowedUserTypes={[1, 2]}
-            element={<AdminMembersPage />}
-          />
-          <ProtectedRoute
-            path="/profilePage"
-            allowedUserTypes={[1, 2]}
-            element={<ProfilePage />}
-          />
-          <ProtectedRoute
-            path="/machinetablePage"
-            allowedUserTypes={[1, 2]}
-            element={<MachinePage />}
-          />
+          <Route path="/viewSeatPage" element={<ViewSeatPage />} />
+          <Route path="/dashboardPage" element={<DashboardPage />} />
+          <Route path="/helpPage" element={<HelpPage />} />
+          <Route path="/projectPage" element={<ProjectPage />} />
+          <Route path="/seatplanPage" element={<SeatplanPage />} />
+          <Route path="/adminPage" element={<AdminMembersPage />} />
+          <Route path="/profilePage" element={<ProfilePage />} />
+          <Route path="/machinetablePage" element={<MachinePage />} />
 
           {/* Redirect to the homepage for unknown routes */}
           <Route path="*" element={<Navigate to="/" />} />
